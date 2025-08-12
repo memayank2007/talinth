c3c05f47540f765228ef68315fee9da3bd99a621
Mayank <mayank.mishra3@globalloigc.com>
2025-08-12 12:18:20 +0530

    updated the theam

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,14 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+
+// Link ONLY vendor/assets/vendor as the root
+//= link_tree ../../../vendor/assets/css .css
+//= link_tree ../../../vendor/assets/js .js
+//= link_tree ../../../vendor/assets/vendor .css
+//= link_tree ../../../vendor/assets/vendor .js
+//= link_tree ../../../vendor/assets/vendor .svg
+//= link_tree ../../../vendor/assets/vendor .png
+//= link_tree ../../../vendor/assets/vendor .jpg
+//= link_tree ../../../vendor/assets/vendor .woff
+//= link_tree ../../../vendor/assets/vendor .woff2
+//= link_tree ../../../vendor/assets/vendor .ttf
---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,9 @@
+class DashboardController < ApplicationController
+
+    def index
+      # fetch stats you want to show
+      # @today_interviews = Interview.today.count
+      # @pass_rate = Interview.pass_rate_last_30_days
+      # ...
+    end
+  end
